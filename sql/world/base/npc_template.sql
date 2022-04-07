50669812e8bceda237f95cba15db8a6cc3616ef9
Pagani Walter <paganiwalter@gmail.com>
2022-04-06 23:46:32 -0300

    (Script/Translations) Modifying texts into multiple languages

--- sql/world/base/npc_template.sql
+++ sql/world/base/npc_template.sql
@@ -1,10 +1,10 @@
 -- NPC TEMPLATE FOR mod-instance-reset
 
 SET
-@Entry 		:= 300000,
-@Model 		:= 24877,
-@Name 		:= "Cromi",
-@Title 		:= "Instance Reset";
+@Entry      := 300000,
+@Model      := 24877,
+@Name       := "Cromi",
+@Title      := "Instance Reset";
 
 -- NPC
 DELETE FROM `creature_template` WHERE `entry` = @Entry;
@@ -12,4 +12,10 @@ DELETE FROM `creature_template` WHERE `entry` = @Entry;
 INSERT INTO `creature_template` (`entry`, `modelid1`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `faction`, `npcflag`, `speed_walk`, `speed_run`, `scale`, `rank`, `unit_class`, `unit_flags`, `type`, `type_flags`, `RegenHealth`, `flags_extra`, `ScriptName`) VALUES
 (@Entry, @Model, @Name, @Title, "Speak", 0, 80, 80, 35, 0, 1, 1.14286, 1, 0, 1, 2, 7, 138936390, 1, 2, "instanceReset");
 
--- TODO: add gossip text
+UPDATE `creature_template` SET `npcflag`=`npcflag`|1 WHERE `entry`=@Entry;
+
+DELETE FROM `creature_template_locale` WHERE `entry`=@Entry AND `locale` IN ('esES', 'esMX');
+
+INSERT INTO `creature_template_locale` (`entry`, `locale`, `Name`, `Title`, `VerifiedBuild`) VALUES
+(@Entry, 'esES', @Name, 'Reinicio de instancias', 0),
+(@Entry, 'esMX', @Name, 'Reinicio de instancias', 0);
--- src/InstanceReset.cpp
+++ src/InstanceReset.cpp
@@ -5,9 +5,70 @@
 #include "GossipDef.h"
 #include "ScriptedGossip.h"
 #include "Language.h"
+#include "Chat.h"
+#include "WorldPacket.h"
+#include "WorldSession.h"
+#include "Opcodes.h"
 
 static bool instancereset_enable;
 static bool instancereset_normalmodeonly;
+
+void GossipSetText(Player* player, std::string message, uint32 textID)
+{
+    GossipText const* gossip = sObjectMgr->GetGossipText(textID);
+    WorldPacket data(SMSG_NPC_TEXT_UPDATE, 100);
+    data << textID;
+    for (uint8 i = 0; i < MAX_GOSSIP_TEXT_OPTIONS; ++i)
+    {
+        data << float(0);
+        data << message;
+        data << message;
+        data << uint32(0);
+        data << uint32(0);
+        data << uint32(0);
+        data << uint32(0);
+        data << uint32(0);
+        data << uint32(0);
+        data << uint32(0);
+    }
+    player->GetSession()->SendPacket(&data);
+}
+
+class InstanceResetAnnouncer : public PlayerScript
+{
+public:
+    InstanceResetAnnouncer() : PlayerScript("InstanceResetAnnouncer") {}
+
+    void OnLogin(Player* player)
+    {
+        if (sConfigMgr->GetOption<bool>("instanceResetAnnouncer.announceEnable", true))
+        {
+            std::string message = "";
+            switch (player->GetSession()->GetSessionDbLocaleIndex())
+            {
+                case LOCALE_enUS:
+                case LOCALE_koKR:
+                case LOCALE_frFR:
+                case LOCALE_deDE:
+                case LOCALE_zhCN:
+                case LOCALE_zhTW:
+                case LOCALE_ruRU:
+                {
+                    message = "This server is running the |cff4CFF00Instance Reset |rmodule.";
+                    break;
+
+                }
+                case LOCALE_esES:
+                case LOCALE_esMX:
+                {
+                    message = "Este servidor está ejecutando el módulo |cff4CFF00Instance reset|r";
+                    break;
+                }
+            }
+            ChatHandler(player->GetSession()).SendSysMessage(message);
+        }
+    }
+};
 
 class instanceResetConfigLoad : public WorldScript {
 public:
@@ -29,7 +90,33 @@ public:
     {
         if (instancereset_enable) {
             ClearGossipMenuFor(player);
-            AddGossipItemFor(player, GOSSIP_ICON_CHAT, "I would like to remove my instance saves.", GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
+            std::string gossipText = "";
+            std::string message = "";
+            switch (player->GetSession()->GetSessionDbLocaleIndex())
+            {
+                case LOCALE_enUS:
+                case LOCALE_koKR:
+                case LOCALE_frFR:
+                case LOCALE_deDE:
+                case LOCALE_zhCN:
+                case LOCALE_zhTW:
+                case LOCALE_ruRU:
+                {
+                    gossipText = "I would like to remove my instance saves.";
+                    message = "Greetings $n. This is an npc that allows you to reset instance ids, allowing you to re-enter, without the need to wait for the reset time to expire. It was developed by the AzerothCore community.";
+                    break;
+
+                }
+                case LOCALE_esES:
+                case LOCALE_esMX:
+                {
+                    gossipText = "Me gustaría reiniciar mis ids de instancias.";
+                    message = "Saludos $n. Este es un npc que te permite reiniciar los ids de las instancias, permitiéndote volver a entrar, sin la necesidad de esperar a que se cumpla el tiempo para el reinicio. Fue desarrollado por la comunidad de AzerothCore.";
+                    break;
+                }
+            }
+            GossipSetText(player, message, DEFAULT_GOSSIP_MESSAGE);
+            AddGossipItemFor(player, GOSSIP_ICON_CHAT, gossipText, GOSSIP_SENDER_MAIN, GOSSIP_ACTION_INFO_DEF + 1);
             SendGossipMenuFor(player, DEFAULT_GOSSIP_MESSAGE, creature->GetGUID());
         }
         return true;
@@ -57,7 +144,28 @@ public:
                         ++itr;
                 }
             }
-            creature->Whisper("Your instances have been reset.", LANG_UNIVERSAL, player);
+            std::string creatureWhisper = "";
+            switch (player->GetSession()->GetSessionDbLocaleIndex())
+            {
+                case LOCALE_enUS:
+                case LOCALE_koKR:
+                case LOCALE_frFR:
+                case LOCALE_deDE:
+                case LOCALE_zhCN:
+                case LOCALE_zhTW:
+                case LOCALE_ruRU:
+                {
+                    creatureWhisper = "Your instances have been reset.";
+                    break;
+                }
+                case LOCALE_esES:
+                case LOCALE_esMX:
+                {
+                    creatureWhisper = "Sus instancias han sido restablecidas.";
+                    break;
+                }
+            }
+            creature->Whisper(creatureWhisper, LANG_UNIVERSAL, player);
             CloseGossipMenuFor(player);
         }
         return true;
@@ -67,5 +175,5 @@ public:
 void AddInstanceResetScripts() {
     new instanceResetConfigLoad();
     new instanceReset();
+    new InstanceResetAnnouncer();
 }
-
